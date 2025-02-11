84025caebcf142862a26ae87051cd45848285116
Luthfi <luthfihadi543@gmail.com>
2025-02-11 16:07:52 +0700

    fix: navbar responivities

--- frontend/src/components/Layout/Navbar.jsx
+++ frontend/src/components/Layout/Navbar.jsx
@@ -10,6 +10,7 @@ import {
   MenuItem,
   Avatar,
   useColorModeValue,
+  Container,
 } from '@chakra-ui/react';
 import { FiChevronDown } from 'react-icons/fi';
 import { useAuth } from '../../contexts/AuthContext';
@@ -20,32 +21,34 @@ const Navbar = () => {
 
   return (
     <Box bg={bgColor} px={4} shadow="sm">
-      <Flex h={16} alignItems="center" justifyContent="space-between">
-        <Text fontSize="xl" fontWeight="bold" color="blue.500">
-          Kanban Board
-        </Text>
+      <Container maxW="container.xl">
+        <Flex h={16} alignItems="center" justifyContent="space-between">
+          <Text fontSize="xl" fontWeight="bold" color="blue.500">
+            Kanban Board
+          </Text>
 
-        <Menu>
-          <ChakraMenuButton
-            as={Button}
-            variant="ghost"
-            rightIcon={<FiChevronDown />}
-            padding={2}
-          >
-            <Flex alignItems="center">
-              <Avatar
-                size="sm"
-                name={user?.name}
-                marginRight={2}
-              />
-              <Text>{user?.name}</Text>
-            </Flex>
-          </ChakraMenuButton>
-          <MenuList>
-            <MenuItem onClick={logout}>Logout</MenuItem>
-          </MenuList>
-        </Menu>
-      </Flex>
+          <Menu>
+            <ChakraMenuButton
+              as={Button}
+              variant="ghost"
+              rightIcon={<FiChevronDown />}
+              padding={2}
+            >
+              <Flex alignItems="center">
+                <Avatar
+                  size="sm"
+                  name={user?.name}
+                  marginRight={2}
+                />
+                <Text>{user?.name}</Text>
+              </Flex>
+            </ChakraMenuButton>
+            <MenuList>
+              <MenuItem onClick={logout}>Logout</MenuItem>
+            </MenuList>
+          </Menu>
+        </Flex>
+      </Container>
     </Box>
   );
 };
